39c4f70a440ef5e69a6ec86babff16c1c2a45f86
HopeEternal <risalee316@gmail.com>
2020-02-10 12:28:23 -0500

    Card Component Completed and data imported

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Hope Eternal',
+    username: 'Hope1337',
+    email: 'Sincere@april.biz'
+  },
+  { 
+    id: 2,
+    name: 'WilsonVolleybot',
+    username: 'Willy3113',
+    email: 'the.real.castaway@hanks.com'
+  },
+  {
+    id: 3,
+    name: 'Calculon',
+    username: 'AllMyCircuits',
+    email: 'coma@futurama.net'
+  },
+  {
+    id: 4,
+    name: 'Bender Bending Rodríguez',
+    username: 'ButMyShinyMetalCSSClass',
+    email: 'bending.unit22@futurama.org'
+  },
+  {
+    id: 5,
+    name: 'Hedonismbot',
+    username: 'GrapeFiend',
+    email: 'Rome.Lives@futurama.it'
+  },
+  {
+    id: 6,
+    name: 'Roberto',
+    username: 'StabboBot',
+    email: 'made.of.tuesday@futurama.info'
+  },
+  {
+    id: 7,
+    name: 'Robot Santa Claus',
+    username: 'HoHoExploder',
+    email: 'north-pole-terror@futurama.biz'
+  },
+  {
+    id: 8,
+    name: 'Animatronio',
+    username: 'DavincisMasterpiece',
+    email: 'overshares@futurama.me'
+  },
+  {
+    id: 9,
+    name: 'R2D2',
+    username: 'BloopBleepBlorp',
+    email: 'skywalker@luke.io'
+  },
+  {
+    id: 10,
+    name: 'C3PO',
+    username: 'GoldIsTheNewBlack',
+    email: 'irritatingly_cautious@lucas.org'
+  }
+];
